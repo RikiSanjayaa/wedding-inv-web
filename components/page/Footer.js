5bb97a77ed7ec824852a1fb3104379863b8da1f4
dekca445 <suthawijaya445@gmail.com>
2024-12-13 17:13:08 +0700

    menambahkan breakpoint2 pada footer

--- components/page/Footer.js
+++ components/page/Footer.js
@@ -1,20 +1,38 @@
+import Breakpoints2 from "../utilities/Breakpoints2";
+
 export default function Footer() {
   return (
-    <div 
-      className="relative inset-0 bg-cover bg-center h-44 md:h-60 lg:h-80" 
-      style={{ backgroundImage: 'url(/images/landing_img.png)' }}>
+    <div
+      className="relative inset-0 bg-cover bg-center h-44 md:h-60 lg:h-80"
+      style={{ backgroundImage: 'url(/images/landing_img.png)' }}
+    >
+      {/* Overlay hitam dengan kecerahan 5 */}
       <div className="absolute inset-0 bg-black opacity-5"></div>
-      <div className="h-full w-full flex flex-col justify-center items-center gap-4 relative z-10">
+
+      {/* Konten Utama Footer */}
+      <div className="h-full w-full flex flex-col justify-center items-center gap-6 relative z-10">
+        {/* Bagian Nama dan Garis */}
         <div className="flex items-center gap-4">
-          <div className="flex items-center justify-center px-10 py-2 border border-white rounded-lg">
-            <div className="text-white text-3xl font-normal font-['Allura']">Asep</div>
+          {/* Nama Kiri */}
+          <div className="px-8 py-3 rounded-[10px] border border-white">
+            <div className="text-white text-3xl font-normal font-['Allura'] leading-9">
+              Asep
+            </div>
           </div>
-          <div className="w-4 h-1 bg-white rounded-full"></div>
-          <div className="flex items-center justify-center px-10 py-2 border border-white rounded-lg">
-            <div className="text-white text-3xl font-normal font-['Allura']">Putri</div>
+
+          {/* Garis Tengah */}
+          <div className="h-1 w-8 bg-white rounded-full"></div>
+
+          {/* Nama Kanan */}
+          <div className="px-8 py-3 rounded-[10px] border border-white">
+            <div className="text-white text-3xl font-normal font-['Allura'] leading-9">
+              Putri
+            </div>
           </div>
         </div>
-        <div className="w-[300px] h-1 bg-transparent"></div>
+
+        {/* Garis Sakura */}
+        <Breakpoints2 />
       </div>
     </div>
   );
