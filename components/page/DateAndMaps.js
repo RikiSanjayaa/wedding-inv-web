421724d83085d54367078717fbcdfaefb0689f98
root <root@Server-SCUC.csuc.site>
2024-12-13 07:39:54 +0000

    Update Kata

--- components/page/DateAndMaps.js
+++ components/page/DateAndMaps.js
@@ -46,7 +46,7 @@ export default function DateAndMaps() {
         Assalamualaikum Warrohmatullah Wabarrakatuhu
       </p>
       <p className="text-center text-netral-600 text-base md:text-lg xl:text-xl 2xl:text-2xl">
-        By asking for the grace and blessing of Allah SWT. We intend to hold a wedding celebration for our sons and daughters, which Allah SWT willing will be held on:
+      Pernikahan adalah ibadah, dan setiap ibadah bermuara pada cinta-Nya sebagai tujuan. Sudah sewajarnya setiap upaya meraih cinta-Nya dilakukan dengan sukacita.
       </p>
       <p className="leading-4 text-center text-netral-500 text-xl md:text-2xl xl:text-3xl 2xl:text-4xl">25 January 2025</p>
       <div className="flex gap-4 md:gap-6 lg:gap-8 xl:gap-10 justify-center">
@@ -57,7 +57,7 @@ export default function DateAndMaps() {
       </div>
       <div className="flex flex-col md:flex-row gap-10 w-full">
         <MapCard
-          title="Wedding Ceremony"
+          title="AKAD"
           time="07.00 - 09.00"
           place="Islamic Centre"
           address="Jl. Udayana, Gomong, Kec. Selaparang, Kota Mataram, Nusa Tenggara Bar."
@@ -66,7 +66,7 @@ export default function DateAndMaps() {
           mapUrl="https://www.google.com/maps/place/Masjid+Raya+Hubbul+Wathan+-+Islamic+Center+NTB/@-8.5799593,116.0980343,17z/data=!3m1!4b1!4m6!3m5!1s0x2dcdc09aed999edd:0x3bc68fa9a57918a3!8m2!3d-8.5799646!4d116.1006092!16s%2Fg%2F11cn5r3pkl?entry=ttu&g_ep=EgoyMDI0MTIwOS4wIKXMDSoASAFQAw%3D%3D"
         />
         <MapCard
-          title="Wedding Party"
+          title="RESEPSI"
           time="11.00 - 14.00"
           place="Prime Park Hotel"
           address="Jl. Udayana, Monjok, Kec. Selaparang, Kota Mataram, Nusa Tenggara Bar."
@@ -76,7 +76,7 @@ export default function DateAndMaps() {
         />
       </div>
       <p className="text-center text-netral-600 text-base md:text-lg xl:text-xl 2xl:text-2xl">
-        It is an honor and happiness for us if Mr / Ms / Brother (i) is willing to attend to give blessings to the bride and groom.
+      suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara(i) berkenan hadir untuk memberikan berkat kepada kedua mempelai..
       </p>
     </div>
   )
