ff469244319a3b22c69503ec10f57e7d9f1536b5
msaririzki <msaririzki12@gmail.com>
2024-12-13 16:03:56 +0000

    Update deskripsi dan music

--- components/page/StorySection.js
+++ components/page/StorySection.js
@@ -11,7 +11,7 @@ export default function StorySection() {
         <div className="p-10 w-full bg-white rounded-lg overflow-hidden shadow-soft-sakura transform transition duration-500 hover:scale-105 flex items-center flex-col justify-center gap-5 md:order-2">
           <p className="font-bold text-center text-netral-600 text-base md:text-lg xl:text-xl 2xl:text-2xl">Agustus, 2020</p>
           <p className="text-center text-netral-600 text-base md:text-lg xl:text-xl 2xl:text-2xl">
-          Kisah kami dimulai di sebuah kampus ternama saat kami pertama kali bertemu dalam kegiatan orientasi mahasiswa. Tika, mahasiswa Psikologi, dan Anugrah, mahasiswa Teknik, saling berkenalan secara singkat. Tak lama setelahnya, kami dipertemukan kembali dalam proyek kolaborasi antar fakultas, yang membuat kami semakin dekat. Pada Maret 2022, kami memutuskan untuk menjalin hubungan yang lebih serius, saling mendukung dan menjalani perjalanan kuliah bersama.
+          Kisah kami dimulai di sebuah kampus ternama saat kami pertama kali bertemu dalam kegiatan orientasi mahasiswa. Putri, mahasiswa Psikologi, dan Asep, mahasiswa Teknik, saling berkenalan secara singkat. Tak lama setelahnya, kami dipertemukan kembali dalam proyek kolaborasi antar fakultas, yang membuat kami semakin dekat. Pada Maret 2022, kami memutuskan untuk menjalin hubungan yang lebih serius, saling mendukung dan menjalani perjalanan kuliah bersama.
           </p>
         </div>
         <div className="w-full bg-white rounded-lg overflow-hidden shadow-soft-sakura transform transition duration-500 hover:scale-105 md:order-4">
